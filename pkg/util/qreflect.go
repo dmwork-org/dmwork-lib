566a248fbd0108799e10a390cda60a83d22a9f19
mininglamp <mlamp@mininglampdeMac-mini-2.local>
2026-03-14 06:12:48 +0800

    fix: add input validation to ZAdd and qreflect to prevent panics

--- pkg/util/qreflect.go
+++ pkg/util/qreflect.go
@@ -1,13 +1,26 @@
 package util
 
 import (
+	"errors"
 	"reflect"
 )
 
 // AttrToUnderscore 获取struct的所有属性并转为下划线模式
-func AttrToUnderscore(st interface{}) []string {
+func AttrToUnderscore(st interface{}) ([]string, error) {
+	if st == nil {
+		return nil, errors.New("input cannot be nil")
+	}
 	t := reflect.ValueOf(st)
+	if t.Kind() != reflect.Ptr {
+		return nil, errors.New("input must be a pointer to struct")
+	}
+	if t.IsNil() {
+		return nil, errors.New("input cannot be nil pointer")
+	}
 	vType := t.Elem().Type()
+	if vType.Kind() != reflect.Struct {
+		return nil, errors.New("input must be a pointer to struct")
+	}
 	names := make([]string, 0)
 	for i := 0; i < vType.NumField(); i++ {
 		if vType.Field(i).Type.Kind() == reflect.Struct {
@@ -18,5 +31,5 @@ func AttrToUnderscore(st interface{}) []string {
 			names = append(names, UnderscoreName(name))
 		}
 	}
-	return names
+	return names, nil
 }
